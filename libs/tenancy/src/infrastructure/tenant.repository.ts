9705defb6f150010a8bc397609a2cff65e5ef4f0
akeemamuni <amuni.engr@gmail.com>
2025-07-03 17:41:47 +0000

    Tenancy infra layer

---
+++ libs/tenancy/src/infrastructure/tenant.repository.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { Tenant } from '../domain/tenant.entity';
+
+// cont step 5. Tenant Repository (Infrastructure Layer)
