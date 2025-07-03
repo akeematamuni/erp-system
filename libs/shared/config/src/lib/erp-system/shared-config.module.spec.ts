bf36d5519b47ea1743416654cfb0520ff54e30c5
akeemamuni <amuni.engr@gmail.com>
2025-07-03 10:53:00 +0000

    Unit test for shared config

---
+++ libs/shared/config/src/lib/erp-system/shared-config.module.spec.ts
@@ -0,0 +1,13 @@
+import { configSchema } from './config.schema';
+
+describe('Config Schema', () => {
+    it('validates env variable', () => {
+        const PORT = '4000';
+        const GLOBAL_PREFIX = 'api/test';
+
+        const parsed = configSchema.safeParse({ PORT, GLOBAL_PREFIX });
+
+        expect(parsed.success).toBe(true);
+        expect(parsed.success && parsed.data.PORT).toBe(4000);
+    });
+});
