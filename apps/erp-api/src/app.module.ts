cb0c9f1c034a508c42261352633549c626bf0c77
akeemamuni <amuni.engr@gmail.com>
2025-07-04 16:01:40 +0000

    Tenancy and database wired into app module

--- apps/erp-api/src/app.module.ts
+++ apps/erp-api/src/app.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './presentation/app.controller';
 import { AppService } from './application/app.service';
 import { ErpSystemSharedConfigModule } from '@erp-system/shared-config';
+import { SharedDatabaseModule } from '@erp-system/shared-database';
+import { TenancyModule } from '@erp-system/tenancy';
+import { TenantMiddleware } from '@erp-system/tenancy';
 
 @Module({
     imports: [
-        ErpSystemSharedConfigModule
+        ErpSystemSharedConfigModule,
+        SharedDatabaseModule,
+        TenancyModule
     ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+    // Apply tenant middleware on all routes
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(TenantMiddleware).forRoutes('*'); // Reg. and login to be excluded
+    }
+}
