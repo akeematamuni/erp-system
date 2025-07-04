b9981037e9f0bec4da3398ea6b46cb4b5e3e00a3
akeemamuni <amuni.engr@gmail.com>
2025-07-04 15:57:17 +0000

    Database URL added to schema

--- libs/shared/config/src/config/config.schema.ts
+++ libs/shared/config/src/config/config.schema.ts
@@ -3,4 +3,5 @@ import { z } from 'zod'
 export const configSchema = z.object({
     PORT: z.coerce.number().default(3000),
     GLOBAL_PREFIX: z.coerce.string().default('api'),
+    DATABASE_URL: z.coerce.string()
 });
