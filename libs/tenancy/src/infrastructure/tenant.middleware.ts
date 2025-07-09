7b2ae4f7aa8afc673e4d997c6b1f5965ea0b2508
akeemamuni <amuni.engr@gmail.com>
2025-07-09 13:48:24 -0400

    update: refactored tenant middleware to utilize adaptable logger

--- libs/tenancy/src/infrastructure/tenant.middleware.ts
+++ libs/tenancy/src/infrastructure/tenant.middleware.ts
@@ -1,22 +1,37 @@
 import { 
-    Injectable, NestMiddleware, 
-    UnauthorizedException, NotFoundException 
+    Injectable, NestMiddleware, UnauthorizedException, 
+    NotFoundException, LoggerService, Inject
 } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { TenantService } from '../application/tenant.service';
+import { LoggerToken, ContextualLoggerService } from '@erp-system/shared-logger';
 
-// Tenant middleware to resolve tenant's schema
+// Middleware to resolve tenant's schema
 @Injectable()
 export class TenantMiddleware implements NestMiddleware {
-    constructor(private readonly tenantService: TenantService) {}
+    private readonly logger: LoggerService;
+
+    constructor(
+        @Inject(LoggerToken) base: ContextualLoggerService,
+        private readonly tenantService: TenantService
+    ) {
+        this.logger = base.addContext(TenantMiddleware.name);
+    }
 
     async use(req: Request, res: Response, next: NextFunction) {
         const tenantId = req.headers['x-tenant-id'] as string;
-        if (!tenantId) throw new UnauthorizedException('Tenant ID must be provided');
+        if (!tenantId) {
+            this.logger.warn('Tenant ID was not provided..')
+            throw new UnauthorizedException('Tenant ID must be provided..');
+        }
 
         const tenant = await this.tenantService.resolveTenant(tenantId);
-        if (!tenant) throw new NotFoundException('Tenant not found');
+        if (!tenant) {
+            this.logger.warn(`Tenant with ID: ${tenantId} was not not found..`);
+            throw new NotFoundException('Tenant not found..');
+        }
 
+        this.logger.log(`Tenant "${tenantId}" schema resolved to ${tenant.schema}`);
         await this.tenantService.setSearchPath(tenant.schema);
         (req as any).tenant = tenant;
 
