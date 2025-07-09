9ce01946e2ba319bee328ce1ceb456b2d5788233
akeemamuni <amuni.engr@gmail.com>
2025-07-09 13:40:54 -0400

    update: logger_type added to config schema

--- libs/shared/config/src/config/config.schema.ts
+++ libs/shared/config/src/config/config.schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const configSchema = z.object({
     PORT: z.coerce.number().default(3000),
+    LOGGER_TYPE: z.coerce.string().default('nest'),
     GLOBAL_PREFIX: z.coerce.string().default('api'),
-    DATABASE_URL: z.coerce.string()
+    DATABASE_URL: z.coerce.string(),
 });
