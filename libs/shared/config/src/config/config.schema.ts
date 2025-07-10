11c979bfb58d119bc6323d095a7ee4610f87e32a
akeemamuni <amuni.engr@gmail.com>
2025-07-10 15:46:12 -0400

    update: some defaults removed

--- libs/shared/config/src/config/config.schema.ts
+++ libs/shared/config/src/config/config.schema.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 export const configSchema = z.object({
     PORT: z.coerce.number().default(3000),
     LOGGER_TYPE: z.coerce.string().default('nest'),
-    LOGGER_FORMAT: z.coerce.string().default('plain'),
-    LOGGER_OUTPUT: z.coerce.string().default('console'),
     GLOBAL_PREFIX: z.coerce.string().default('api'),
-    DATABASE_URL: z.coerce.string()
+    LOGGER_FORMAT: z.coerce.string(),
+    LOGGER_OUTPUT: z.coerce.string(),
+    DATABASE_URL: z.coerce.string(),
 });
