0e1eefcb3d16350e9cbd30b1daa16624e5c2901a
akeemamuni <amuni.engr@gmail.com>
2025-07-10 10:31:02 -0400

    update: contextual logger changed to custom logger

--- libs/shared/logger/src/ports/custom-logger.service.ts
+++ libs/shared/logger/src/ports/custom-logger.service.ts
@@ -1,6 +1,6 @@
 import { LoggerService } from '@nestjs/common';
 
 // Create an interface that would allow LoggerService use context
-export interface ContextualLoggerService extends LoggerService {
+export interface CustomLoggerService extends LoggerService {
     addContext(context: string): LoggerService;
 }
