84d8f0e2818a1ab9431c564592112f74b6649e61
akeemamuni <amuni.engr@gmail.com>
2025-07-16 12:07:37 -0400

    add: shared auth infrastructure layer housing domain implementation

---
+++ libs/shared/auth/src/infrastructure/user.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
+import { User } from '../domain/user.entity';
+import { IUserRepository } from '../domain/user.repository.interface';
+
+@Injectable()
+export class UserRepository implements IUserRepository {
+    constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
+
+    async save(user: User): Promise<User> {
+        return await this.repo.save(user);
+    }
+
+    async findById(id: string): Promise<User | null> {
+        return await this.repo.findOne({ where: { id } });
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        return await this.repo.findOne({ where: { email } });
+    }
+
+    async findByDepartment(department: string): Promise<User[]> {
+        return await this.repo.find({ where: { department } });
+    }
+
+    async userExist(email: string): Promise<boolean> {
+        return await this.repo.count({ where: { email } }) > 0;
+    }
+}
