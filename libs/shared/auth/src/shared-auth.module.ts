de839d7452987ce67926740683911d029c4ba352
akeemamuni <amuni.engr@gmail.com>
2025-07-18 17:17:29 -0400

    update: wire typeorm into auth module

--- libs/shared/auth/src/shared-auth.module.ts
+++ libs/shared/auth/src/shared-auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './domain/user.entity';
+import { UserRepositoryToken } from './user.repository.token';
+import { UserRepository } from './infrastructure/user.repository';
 
 @Module({
-    controllers: [],
-    providers: [],
-    exports: [],
+    imports: [TypeOrmModule.forFeature([User])],
+    providers: [{ provide: UserRepositoryToken, useClass: UserRepository }],
+    exports: [UserRepositoryToken],
 })
 export class SharedAuthModule {}
 // wiring up...
