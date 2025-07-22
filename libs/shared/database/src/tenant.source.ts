87deb90f74fdbbdd99b719ad78edbce886c97c45
akeemamuni <amuni.engr@gmail.com>
2025-07-22 15:37:50 -0400

    update: tenant data option uses direct migration class

--- libs/shared/database/src/tenant.source.ts
+++ libs/shared/database/src/tenant.source.ts
@@ -3,13 +3,18 @@ import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { User } from '@erp-system/shared-auth';
 import { parsed } from '@erp-system/shared-config';
+import { CreateUserTable1753118629025 } from './migrations/tenant/1753118629025-CreateUserTable';
 
 // Tables to create inside each tenant schema
 export const tenantDataOptions: PostgresConnectionOptions = {
     type: 'postgres',
     url: parsed.data?.DATABASE_URL,
     entities: [User],
-    migrations: ['libs/shared/database/src/migrations/tenant/*.ts']
+    migrations: [CreateUserTable1753118629025],
+    ssl: { rejectUnauthorized: false }
 }
 
-export default new DataSource(tenantDataOptions);
+export default new DataSource({
+    ...tenantDataOptions,
+    migrations: ['libs/shared/database/src/migrations/tenant/*.ts']
+});
