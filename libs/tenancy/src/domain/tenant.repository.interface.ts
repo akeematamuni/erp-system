b6d2a73bc310e10f5ed490cb60f361a1089de482
akeemamuni <amuni.engr@gmail.com>
2025-07-24 11:35:35 -0400

    update: rolling back tenant creation contract

--- libs/tenancy/src/domain/tenant.repository.interface.ts
+++ libs/tenancy/src/domain/tenant.repository.interface.ts
@@ -4,5 +4,6 @@ import { DataSource } from 'typeorm';
 export interface ITenantRepository {
     setSchema(schema: string): Promise<any>;
     findById(id: string): Promise<Tenant|null>;
+    rollbackTenantCreation(schema: string, tenant: Tenant | null): Promise<void>;
     createTenant(name: string): Promise<{ tenant: Tenant; tenantDataSource: DataSource }>;
 }
