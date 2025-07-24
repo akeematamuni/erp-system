d086fd7050c3dab8cb70660e3cad4ce6637c7ad0
akeemamuni <amuni.engr@gmail.com>
2025-07-24 11:40:19 -0400

    update: utilizing proper response dto

--- libs/shared/auth/src/presentation/auth.controller.ts
+++ libs/shared/auth/src/presentation/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from '../application/auth.service';
 import { RegisterTenantDto } from './dto/register-tenant.dto';
+import { RegResponseDto } from './dto/register-response.dto';
+import { plainToInstance, instanceToPlain } from 'class-transformer';
 
 @Controller('auth')
 export class AuthController {
@@ -8,6 +10,11 @@ export class AuthController {
 
     @Post('register')
     async registerTenant(@Body(ValidationPipe) dto: RegisterTenantDto) {
-        return await this.authService.registerTenant(dto);
+        const responce = await this.authService.registerTenant(dto);
+        return plainToInstance(
+            RegResponseDto, 
+            instanceToPlain(responce), 
+            { excludeExtraneousValues: true }
+        );
     }
 }
