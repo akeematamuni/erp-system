a61e0914707a18936fb4bc8d54b92d559517ca26
akeemamuni <amuni.engr@gmail.com>
2025-07-28 15:38:55 -0400

    update: inclusion of default tenant schema

--- libs/shared/database/src/tenant.source.ts
+++ libs/shared/database/src/tenant.source.ts
@@ -3,18 +3,20 @@ import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { User } from '@erp-system/shared-auth';
 import { parsed } from '@erp-system/shared-config';
-import { CreateUserTable1753118629025 } from './migrations/tenant/1753118629025-CreateUserTable';
+import { CreateUserTable1753715874894 } from './migrations/tenant/1753715874894-CreateUserTable';
 
 // Tables to create inside each tenant schema
 export const tenantDataOptions: PostgresConnectionOptions = {
     type: 'postgres',
     url: parsed.data?.DATABASE_URL,
     entities: [User],
-    migrations: [CreateUserTable1753118629025],
+    migrations: [CreateUserTable1753715874894],
     ssl: { rejectUnauthorized: false }
 }
 
-export default new DataSource({
+export const defaultSchema = 'a_default_schema';
+
+export const TenantDataSource = new DataSource({
     ...tenantDataOptions,
-    migrations: ['libs/shared/database/src/migrations/tenant/*.ts']
+    schema: defaultSchema
 });
