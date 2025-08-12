1fa7f95e98dfe754be7ac67929a0525b7b70e771
akeemamuni <amuni.engr@gmail.com>
2025-08-12 09:25:16 -0400

    add: users lib sample controller

---
+++ libs/users/src/presentation/user.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe, Ip } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserResponseDto } from './dto/create-user-res.dto';
+import { UserService } from '../application/user.service';
+import { JwtGuard } from '@erp-system/shared-token';
+import { Roles, RolesGuard } from '@erp-system/shared-rbac';
+import { RoleType } from '@erp-system/shared-types';
+import { Request } from 'express';
+
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post('add-new-user')
+    @Roles(RoleType.SUPER_ADMIN, RoleType.ADMIN)
+    @UseGuards(JwtGuard, RolesGuard)
+    async createNewUser(
+        @Ip() ip: string,
+        @Req() req: Request,
+        @Body(ValidationPipe) createUserDto: CreateUserDto
+    ) {
+        const adminUser = req.user as {
+            id: string,
+            role: string,
+            orgId: string
+        }
+
+        const newUserDto = {
+            ...createUserDto,
+            tenantId: adminUser.orgId,
+            createdBy: adminUser.id
+        }
+
+        return await this.userService.adminCreateNewUser(newUserDto);
+    }
+}
