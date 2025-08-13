6a61d070b938e79e2e7269ec133c992cbb87cb46
akeemamuni <amuni.engr@gmail.com>
2025-08-13 14:41:51 -0400

    update: tenancy contract redefined

--- libs/tenancy/src/domain/tenant.repository.interface.ts
+++ libs/tenancy/src/domain/tenant.repository.interface.ts
@@ -6,9 +6,9 @@ export interface ITenantRepository {
     setSchema(schema: string): Promise<any>;
     findTenantById(id: string): Promise<Tenant|null>;
     createTenant(name: string): Promise<{ 
-        tenant: Tenant; tenantDataSource: DataSource, dataSource: DataSource
+        tenant: Tenant, dataSource: DataSource
     }>;
     createPublicUser(
         fullName: string, email: string, password: string, tenantId: string
-    ): Promise<PublicUser>;
+    ): Promise<PublicUser|null>;
 }
