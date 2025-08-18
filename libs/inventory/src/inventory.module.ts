1b3f92ee50baf66c5d270137fc8ea33203402fe8
akeemamuni <amuni.engr@gmail.com>
2025-08-18 15:39:50 -0400

    update: inventory providers and exports

--- libs/inventory/src/inventory.module.ts
+++ libs/inventory/src/inventory.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { InvCategoryRepository } from './infrastructure/category.repository';
+import { InvWarehouseRepository} from './infrastructure/warehouse.repository';
 
 @Module({
     controllers: [],
-    providers: [],
-    exports: [],
+    providers: [
+        InvCategoryRepository,
+        InvWarehouseRepository
+    ],
+    exports: [
+        InvCategoryRepository,
+        InvWarehouseRepository
+    ],
 })
 export class InventoryModule {}
